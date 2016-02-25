8ec252c331fa9a28d82c54655e17ca84e7e8c1b2
caimin <18817507440@163.com>
2016-02-25 17:50:37 +0800

    init

---
+++ Assets/clickTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class clickTest : MonoBehaviour {
+	
+	public void Log() {
+		Debug.Log("pressed!!!");
+	}
+}
